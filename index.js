2f647ac6dbcd5838899fb3fcd9b931dc734c6ffe
Jayanthi <jayanthi.ragu93@gmail.com>
2021-06-08 16:59:18 +0530

    'first'

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 class SortedList {
-  constructor() {
   
+  constructor() {
+    this.items=[];
+    this.length=0;
   }
-  add() {
-   
+  add(item) {
+      this.items.push(item);
+      this.items.sort((a,b)=>a>b?1:-1);
+      this.length=this.length+1;
   }
-  get() {
-
+  get(pos) {
+    if(pos>=this.length){
+      throw new Error('outOfBounds')
+      }
+      else{
+      return this.items[pos];
+      }
   }
   max() {
-
+    if(this.length===0)
+    {
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.max(...this.items);
+    }
   }
   min() {
-
+    if(this.length==0)
+    {
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.min(...this.items);
+    }
   }
   avg() {
-    
-   
+    if(this.length==0)
+    {
+      throw new Error('EmptySortedList');
+    }
+    else{
+    const sum=this.items.reduce((total,value)=>{
+      console.log(total);
+      console.log(value);
+      return total+value
+      },0)
+      return (sum/this.items.length);
+    }
   }
 
   sum() {
-  
+    const sum=this.items.reduce((total,value)=>{
+      return total+value
+      },0)
+      return sum
   }
   
 };
 
+const object=new SortedList();
 module.exports = SortedList;
